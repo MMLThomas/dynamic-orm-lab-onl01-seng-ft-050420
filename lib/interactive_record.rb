a55d8cb79377b207beeba8e7573a7ce71c1f5286
michael thomas <michael.thomas2014@gmail.com>
2020-06-09 20:30:51 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,9 +55,16 @@ class InteractiveRecord
       DB[:conn].execute("SELECT * FROM #{table_name} WHERE name = ?", name)
   end
   
-  def self.find_by(atr)
-    #binding.pry
-    DB[:conn].execute("SELECT * FROM #{table_name} WHERE #{atr.keys.first} = #{atr.values.first}") 
+  # def self.find_by(atr)
+  #   #binding.pry
+  #   DB[:conn].execute("SELECT * FROM #{table_name} WHERE #{atr.keys.first} = #{atr.values.first}") 
+  # end
+  
+  def self.find_by(attribute_hash)
+    value = attribute_hash.values.first
+    formatted_value = value.class == Fixnum || Float ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+    DB[:conn].execute(sql)
   end
   
 
