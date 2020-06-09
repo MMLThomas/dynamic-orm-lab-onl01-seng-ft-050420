0200945087d1808adf145be3003b7610338e8ff1
michael thomas <michael.thomas2014@gmail.com>
2020-06-09 20:10:09 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,7 @@ class InteractiveRecord
   
   def self.find_by(atr)
     #binding.pry
-    DB[:conn].execute("SELECT atr FROM #{table_name} WHERE ", ) 
+    DB[:conn].execute("SELECT atr FROM #{table_name}") 
     
   end
   
